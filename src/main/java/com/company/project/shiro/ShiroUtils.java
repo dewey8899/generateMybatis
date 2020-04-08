c1f18bcb14e71818c07734bcc360d22b1ab7caf7
dewey <1246818050@qq.com>
2020-04-08 15:05:42 +0800

    shiro

--- src/main/java/com/company/project/shiro/ShiroUtils.java
+++ src/main/java/com/company/project/shiro/ShiroUtils.java
@@ -1,65 +1,64 @@
-//package com.company.project.shiro;
-//
-//import com.company.project.model.TbTbUser;
-//import com.company.project.model.TbUser;
-//import org.apache.shiro.SecurityUtils;
-//import org.apache.shiro.authc.Authenticator;
-//import org.apache.shiro.authc.LogoutAware;
-//import org.apache.shiro.session.Session;
-//import org.apache.shiro.subject.SimplePrincipalCollection;
-//import org.apache.shiro.subject.support.DefaultSubjectContext;
-//import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-//import org.crazycake.shiro.RedisSessionDAO;
-//
-//import java.util.Collection;
-//import java.util.Objects;
-//
-///**
-// * @author dewey.du
-// * @create 2020/4/6 0006
-// */
-//
-//public class ShiroUtils {
-//
-//    /**
-//     * 私有构造器
-//     **/
-//    private ShiroUtils() {
-//    }
-//
+package com.company.project.shiro;
+
+import com.company.project.model.TbUser;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.Authenticator;
+import org.apache.shiro.authc.LogoutAware;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.apache.shiro.subject.support.DefaultSubjectContext;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.crazycake.shiro.RedisSessionDAO;
+
+import java.util.Collection;
+import java.util.Objects;
+
+/**
+ * @author dewey.du
+ * @create 2020/4/6 0006
+ */
+
+public class ShiroUtils {
+
+    /**
+     * 私有构造器
+     **/
+    private ShiroUtils() {
+    }
+
 //    private static RedisSessionDAO redisSessionDAO = SpringUtil.getBean(RedisSessionDAO.class);
-//
-//    /**
-//     * 获取当前用户Session
-//     *
-//     * @Return SysTbUserEntity 用户信息
-//     */
-//    public static Session getSession() {
-//        return SecurityUtils.getSubject().getSession();
-//    }
-//
-//    /**
-//     * 用户登出
-//     */
-//    public static void logout() {
-//        SecurityUtils.getSubject().logout();
-//    }
-//
-//    /**
-//     * 获取当前用户信息
-//     *
-//     * @Return SysTbUserEntity 用户信息
-//     */
-//    public static TbUser getTbUserInfo() {
-//        return (TbUser) SecurityUtils.getSubject().getPrincipal();
-//    }
-//
-//    /**
-//     * 删除用户缓存信息
-//     *
-//     * @Param username  用户名称
-//     * @Param isRemoveSession 是否删除Session，删除后用户需重新登录
-//     */
+
+    /**
+     * 获取当前用户Session
+     *
+     * @Return SysTbUserEntity 用户信息
+     */
+    public static Session getSession() {
+        return SecurityUtils.getSubject().getSession();
+    }
+
+    /**
+     * 用户登出
+     */
+    public static void logout() {
+        SecurityUtils.getSubject().logout();
+    }
+
+    /**
+     * 获取当前用户信息
+     *
+     * @Return SysTbUserEntity 用户信息
+     */
+    public static TbUser getTbUserInfo() {
+        return (TbUser) SecurityUtils.getSubject().getPrincipal();
+    }
+
+    /**
+     * 删除用户缓存信息
+     *
+     * @Param username  用户名称
+     * @Param isRemoveSession 是否删除Session，删除后用户需重新登录
+     */
 //    public static void deleteCache(String username, boolean isRemoveSession) {
 //        //从缓存中获取Session
 //        Session session = null;
@@ -98,12 +97,12 @@
 //        Authenticator authc = securityManager.getAuthenticator();
 //        ((LogoutAware) authc).onLogout((SimplePrincipalCollection) attribute);
 //    }
-//
-//    /**
-//     * 从缓存中获取指定用户名的Session
-//     *
-//     * @param username
-//     */
+
+    /**
+     * 从缓存中获取指定用户名的Session
+     *
+     * @param username
+     */
 //    private static Session getSessionByTbUsername(String username) {
 //        // 获取当前已登录的用户session列表
 //        Collection<Session> sessions = redisSessionDAO.getActiveSessions();
@@ -125,5 +124,5 @@
 //        }
 //        return null;
 //    }
-//
-//}
+
+}
